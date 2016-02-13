cf4ed205993ab04098a671b78431d98ffa67f223
Taz Jackson <tatjsn@users.noreply.github.com>
2016-02-14 01:09:38 +0900

    Use constants

--- src/js/reducers.js
+++ src/js/reducers.js
@@ -1,13 +1,14 @@
 import { combineReducers } from 'redux';
+import { SELECT, REQUEST_BEGIN, REQUEST_END } from './actions';
 import { getStates } from './utils';
 
 const message = (state = "Initial", action) => state;
 
 const unitedStates = (state = getStates(), { type, payload }) => {
   switch (type) {
-    case 'SELECT':
+    case SELECT:
       return [ payload ];
-    case 'REQUEST_END':
+    case REQUEST_END:
       return payload;
     default:
       return state;
@@ -16,9 +17,9 @@ const unitedStates = (state = getStates(), { type, payload }) => {
 
 const loading = (state = false, { type, payload }) => {
   switch (type) {
-    case 'REQUEST_BEGIN':
+    case REQUEST_BEGIN:
       return true;
-    case 'REQUEST_END':
+    case REQUEST_END:
       return false;
     default:
       return state;
